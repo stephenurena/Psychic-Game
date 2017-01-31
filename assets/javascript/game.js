f48c098cc1c5a7d337442eb4bd54282709c69ec5
Stephen Urena <surena@Stephens-MacBook-Air.local>
2017-01-30 22:48:10 -0800

    modified js file to reset letters guessed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,8 +3,6 @@ var selectionArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
         'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
         't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-//Sets the computerGuess variable for future use in function(event)
-var currentLetter;
 
 //Variables for tracking our wins, & losses. They begin at 0 guesses remaining at 10.
 var wins = 0;
@@ -14,33 +12,40 @@ var guessRemaining = 10;
 //Letters guessed array
 var charGuessed = [ ];
 
+var currentLetter = selectionArr[Math.floor(Math.random() * selectionArr.length)];
+
 // function, begins when a key a-z is pressed
 document.onkeyup = function(event) {
 
 	//determine which key was pressed
 	var userGuess = event.key.toLowerCase();
 
-	currentLetter = selectionArr[Math.floor(Math.random() * selectionArr.length)];
-		console.log(currentLetter);
+	// var currentLetter = selectionArr[Math.floor(Math.random() * selectionArr.length)];
+	// 	console.log(currentLetter);
 
 	if (userGuess.match(/^[a-z]$/i)) {
 
 		//if userGuess matches currentLetter
 		if (userGuess === currentLetter){
-			console.log(wins++);
+			wins++;
 			alert("You win!");
+			currentLetter = selectionArr[Math.floor(Math.random() * selectionArr.length)];
+
+
 		}
 		//Id userGuess doesn't match currentLetter
 		if (userGuess != currentLetter) {
-			console.log(guessRemaining--)
+			guessRemaining--;
 		} 
 		//Once guesses remaining hits 0, everything resets, and loss increaments by 1
 		if (guessRemaining <= 0) {
+			alert("You lose!");
 			wins = 0;
-			console.log(losses++);
+			losses++;
 			guessRemaining = 10;
 			charGuessed = [];
-			alert("You lose!");
+			charGuessed.push(userGuess = null);
+			currentLetter = selectionArr[Math.floor(Math.random() * selectionArr.length)];
 		}
 		//grabs keys pressed
 		charGuessed.push(userGuess);
@@ -54,7 +59,7 @@ document.onkeyup = function(event) {
 
 	//create variable html that will eventually track wins, losses, guesses remaining, and characters used.
 	var html = "<p><strong>Wins:</strong> " + wins + "</p>" +
-	"<p><strong>Losses:</strong> " + losses + "</p>" + "<p><strong>Guesses Remaining:</strong> " + guessRemaining + "</p>" + "<p><strong>Letters Guessed:</strong>  </p>" + "<p>" + charGuessed.join(", ") + "</p>"; 
+	"<p><strong>Losses:</strong> " + losses + "</p>" + "<p><strong>Guesses Remaining:</strong> " + guessRemaining + "</p>" + "<p><strong>Letters Guessed:</strong>  </p>" + "<p>" + charGuessed.join("  ") + "</p>"; 
 
 	//display the updates on to our page
 	document.querySelector("#game").innerHTML = html;
